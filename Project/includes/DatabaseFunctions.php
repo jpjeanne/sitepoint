776fcc4daf0821037b979c2e4c0a79510534206f
JP Jeanne <jpjeanne@zemeckis.local>
2018-12-12 20:52:41 -0800

    chapter 7

--- Project/includes/DatabaseFunctions.php
+++ Project/includes/DatabaseFunctions.php
@@ -1,58 +1,115 @@
 <?php
 
 function query($pdo, $sql, $parameters = []) {
-    $query = $pdo->prepare($sql);
-    // the execute() method iterates over the passed in array and
-    // binds values in the statement
-    $query->execute($parameters);
-    return $query;
+	$query = $pdo->prepare($sql);
+	$query->execute($parameters);
+	return $query;
 }
 
-function getJoke($pdo, $id) {
-	//Create the array of `$parameters` for use in the `query` function
-	$parameters = [':id' => $id];
-	//call the query function and provide the `$parameters` array
-	$query = query($pdo, 'SELECT * FROM `joke` WHERE `id` = :id', $parameters);
+
+function total($pdo, $table) {
+	$query = query($pdo, 'SELECT COUNT(*) FROM `' . $table . '`');
+	$row = $query->fetch();
+	return $row[0];
+}
+
+
+
+
+function findById($pdo, $table, $primaryKey, $value) {
+	$query = 'SELECT * FROM `' . $table . '` WHERE `' . $primaryKey . '` = :value';
+
+	$parameters = [
+		'value' => $value
+	];
+
+	$query = query($pdo, $query, $parameters);
+
 	return $query->fetch();
 }
 
-function allJokes($pdo) {
-    $jokes =  query($pdo, 'SELECT `joke`.`id`, `joketext`, `name`, `email`
-            FROM `joke` INNER JOIN `author`
-            ON `authorid` = `author`.`id`');
+function insert($pdo, $table, $fields) {
+	$query = 'INSERT INTO `' . $table . '` (';
 
-    return $jokes->fetchAll();
+	foreach ($fields as $key => $value) {
+		$query .= '`' . $key . '`,';
+	}
+
+	$query = rtrim($query, ',');
+
+	$query .= ') VALUES (';
+
+
+	foreach ($fields as $key => $value) {
+		$query .= ':' . $key . ',';
+	}
+
+	$query = rtrim($query, ',');
+
+	$query .= ')';
+
+	$fields = processDates($fields);
+
+	query($pdo, $query, $fields);
 }
 
-function totalJokes($pdo) {
-    $query = query($pdo, 'SELECT COUNT(*) FROM `joke`');
-    $row = $query->fetch();
-    return $row[0];
+
+function update($pdo, $table, $primaryKey, $fields) {
+
+	$query = ' UPDATE `' . $table .'` SET ';
+
+
+	foreach ($fields as $key => $value) {
+		$query .= '`' . $key . '` = :' . $key . ',';
+	}
+
+	$query = rtrim($query, ',');
+
+	$query .= ' WHERE `' . $primaryKey . '` = :primaryKey';
+
+	//Set the :primaryKey variable
+	$fields['primaryKey'] = $fields['id'];
+
+	$fields = processDates($fields);
+
+	query($pdo, $query, $fields);
 }
 
-function insertJoke($pdo, $joketext, $authorId) {
-    $query = 'INSERT INTO `joke` (`joketext`, `jokedate`, `authorId`)
-              VALUES (:joketext, CURDATE(), :authorId)';
 
-    $parameters = [
-        ':joketext' => $joketext,
-        ':authorId' => $authorId];
 
-    query($pdo, $query, $parameters);
+
+function delete($pdo, $table, $primaryKey, $id ) {
+	$parameters = [':id' => $id];
+
+	query($pdo, 'DELETE FROM `' . $table . '` WHERE `' . $primaryKey . '` = :id', $parameters);
 }
 
-function updateJoke($pdo, $jokeId, $joketext, $authorId) {
-    $parameters = [
-        ':joketext' => $joketext,
-        ':authorId' => $authorId,
-        ':id'       => $jokeId];
 
-    query($pdo, 'UPDATE `joke` SET `authorId` = :authorId,
-     `joketext` = :joketext WHERE `id` = :id', $parameters);
+function findAll($pdo, $table) {
+	$result = query($pdo, 'SELECT * FROM `' . $table . '`');
+
+	return $result->fetchAll();
 }
 
-function deleteJoke($pdo, $id) {
-    $parameters = [':id' => $id];
+function processDates($fields) {
+	foreach ($fields as $key => $value) {
+		if ($value instanceof DateTime) {
+			$fields[$key] = $value->format('Y-m-d');
+		}
+	}
 
-    query($pdo, 'DELETE FROM `joke` WHERE `id` = :id', $parameters);
+	return $fields;
+}
+
+
+function save($pdo, $table, $primaryKey, $record) {
+	try {
+		if ($record[$primaryKey] == '') {
+			$record[$primaryKey] = null;
+		}
+		insert($pdo, $table, $record);
+	}
+	catch (PDOException $e) {
+		update($pdo, $table, $primaryKey, $record);
+	}
 }
--- Project/public/jokes.php
+++ Project/public/jokes.php
@@ -1,22 +1,29 @@
 <?php
-
 try {
-    include __DIR__ . '/../includes/DatabaseConnection.php';
-    include __DIR__ . '/../includes/DatabaseFunctions.php';
+	include __DIR__ . '/../includes/DatabaseConnection.php';
+	include __DIR__ . '/../includes/DatabaseFunctions.php';
 
-    $jokes = allJokes($pdo);
-    $title = 'Joke list';
-    $totalJokes = totalJokes($pdo);
-
-    ob_start();
-    include  __DIR__ . '/../templates/jokes.html.php';
-    $output = ob_get_clean();
+	$result = findAll($pdo, 'joke');
+	$jokes = [];
+	foreach ($result as $joke) {
+		$author = findById($pdo, 'author', 'id', $joke['authorid']);
+		$jokes[] = [
+			'id' => $joke['id'],
+			'joketext' => $joke['joketext'],
+			'jokedate' => $joke['jokedate'],
+			'name' => $author['name'],
+			'email' => $author['email']
+		];
+	}
+	$title = 'Joke list';
+	$totalJokes = total($pdo, 'joke');
+	ob_start();
+	include  __DIR__ . '/../templates/jokes.html.php';
+	$output = ob_get_clean();
 }
 catch (PDOException $e) {
-    $title = 'An error has occurred';
-
-    $output = 'Database error: ' . $e->getMessage() . '
-    in ' . $e->getFile() . ':' . $e->getLine();
+	$title = 'An error has occurred';
+	$output = 'Database error: ' . $e->getMessage() . ' in ' .
+	$e->getFile() . ':' . $e->getLine();
 }
-
 include  __DIR__ . '/../templates/layout.html.php';
