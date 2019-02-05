5e71a692a6b541211040f31bc9a3e624ae10a13c
JP Jeanne <jpjeanne@zemeckis.local>
2019-02-04 19:31:33 -0800

    password hashing

--- Project/classes/Ninja/DatabaseTable.php
+++ Project/classes/Ninja/DatabaseTable.php
@@ -119,6 +119,19 @@ class DatabaseTable
 		return $fields;
 	}
 
+	public function find($column, $value)
+	{
+	    $query = 'SELECT * FROM ' . $this->table . ' WHERE ' . $column . ' = :value';
+
+	    $parameters = [
+	        'value' => $value
+	    ];
+
+	    $query = $this->query($query, $parameters);
+
+	    return $query->fetchAll();
+	}
+
 
 	public function save($record)
 	{
